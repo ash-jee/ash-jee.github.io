996a3e24af5d7be095e59f1ec02d3d8b6938af63
paul-levy <paul.levy@nyu.edu>
2020-05-22 03:02:20 -0400

    really the last try -- back to basics

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,8 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+
+
 (function($) {
 
 	var	$window = $(window),
@@ -21,6 +23,30 @@
 			xsmall:   [ '361px',   '480px'  ],
 			xxsmall:  [ null,      '360px'  ]
 		});
+
+	const indicator = document.querySelector('.nav-indicator');
+	const items = document.querySelectorAll('.nav-item');
+
+	function handleIndicator(el) {
+	    items.forEach(item => {
+		    item.classList.remove('is-active');
+		    item.removeAttribute('style');
+		});
+
+	    indicator.style.width = `${el.offsetWidth}px`;
+	    indicator.style.left = `${el.offsetLeft}px`;
+	    indicator.style.backgroundColor = el.getAttribute('active-color');
+
+	    el.classList.add('is-active');
+	    el.style.color = el.getAttribute('active-color');
+	}
+
+
+	items.forEach((item, index) => {
+		item.addEventListener('click', (e) => { handleIndicator(e.target)});
+		item.classList.contains('is-active') && handleIndicator(item);
+	    });
+
 
 	/**
 	 * Applies parallax scrolling to an element's background image.
