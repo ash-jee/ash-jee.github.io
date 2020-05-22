edf8431143e8a9da5dc479788227757cea75fcb9
Ash Bhattacharjee <bhattacharjee.ash@gmail.com>
2020-05-22 04:09:53 -0400

    come on...

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,10 +10,10 @@
 
 	var	$window = $(window),
 		$body = $('body'),
-		$header = $('#header'),
-		$banner = $('#banner’),
-		$wrapper = $('#wrapper’),
-		$nav = $('#nav’);
+		$header = $(‘#header'),
+		$banner = $(‘#banner’),
+		$wrapper = $(‘#wrapper’),
+		$nav = $(‘#nav’);
 
 	// Breakpoints.
 		breakpoints({
@@ -224,77 +224,6 @@ xs	/**
 			})
 			.children()
 				.wrapInner('<div class="inner"><div>');
-
-        // NEW
-	// Tiles.
-		var $tiles = $('.tiles > article');
-
-		$tiles.each(function() {
-
-			var $this = $(this),
-				$image = $this.find('.image'), $img = $image.find('img'),
-				$link = $this.find('.link'),
-				x;
-
-			// Image.
-
-				// Set image.
-					$this.css('background-image', 'url(' + $img.attr('src') + ')');
-
-				// Set position.
-					if (x = $img.data('position'))
-						$image.css('background-position', x);
-
-				// Hide original.
-					$image.hide();
-
-			// Link.
-				if ($link.length > 0) {
-
-					$x = $link.clone()
-						.text('')
-						.addClass('primary')
-						.appendTo($this);
-
-					$link = $link.add($x);
-
-					$link.on('click', function(event) {
-
-						var href = $link.attr('href');
-
-						// Prevent default.
-							event.stopPropagation();
-							event.preventDefault();
-
-						// Target blank?
-							if ($link.attr('target') == '_blank') {
-
-								// Open in new tab.
-									window.open(href);
-
-							}
-
-						// Otherwise ...
-							else {
-
-								// Start transitioning.
-									$this.addClass('is-transitioning');
-									$wrapper.addClass('is-transitioning');
-
-								// Redirect.
-									window.setTimeout(function() {
-										location.href = href;
-									}, 500);
-
-							}
-
-					});
-
-				}
-
-		});
-	//END NEW
-
 
 	// Header.
 		if ($banner.length > 0
